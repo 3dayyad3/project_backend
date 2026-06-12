2f1ca0b612f4fbf0829a86659d3e1bc783bfea8c
3dayyad3 <numerica.carbon@gmail.com>
2026-06-12 15:56:20 +0700

    U

--- route/ticket.js
+++ route/ticket.js
@@ -1,8 +1,7 @@
 const ticketController = require('../controller/ticket.js');
-const authMiddleware = require('../middleware/auth.js');
 const router = require('express').Router();
 
-router.get('/ticket', authMiddleware.verifyToken, ticketController.getTicket);
+router.get('/ticket', ticketController.getTicket);
 router.get('/ticket/:id', ticketController.getTicketId);
 router.post('/ticket', ticketController.postTicket);
 router.put('/ticket', ticketController.putTicket);

256e4090d0b00843d6b4af7db3e8e7c24dd2335d
gerldd <geraldharefaaa512@gmail.com>
2026-06-26 17:13:24 +0700

    ?

--- controller/page.js
+++ controller/page.js
@@ -1,26 +1,23 @@
 const loginPage = (req, res) => {
-  res.render("auth/login");
+  res.render('auth/login');
 };
 
 const registerPage = (req, res) => {
-  res.render("auth/register");
+  res.render('auth/register');
 };
 
 const rootPage = async (req, res, next) => {
-  req.url = "/login";
+  req.url = '/login';
   next();
 };
 
 const adminPage = (req, res) => {
-  res.render("dashboard/admin");
+  res.render('dashboard/admin');
 };
 
 module.exports = {
   loginPage,
   registerPage,
   rootPage,
-<<<<<<< HEAD
-=======
   adminPage,
->>>>>>> f6e1be190e52da08d7e4a008374154704c99caac
 };

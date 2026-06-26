6ac365caca63ccfd4daf88f6b2176e851390882d
olathia <olathiaminervamaruao@gmail.com>
2026-06-26 17:03:27 +0700

    berhasil.

--- route/homePage.js
+++
@@ -1,8 +0,0 @@
-const homePageControler = require('../controller/homePage.js');
-const router = require('express').Router();
-
-router.get('/', homePageControler.rootPage);
-router.get('/login', homePageControler.homePage);
-router.get('/register', homePageControler.registerPage);
-
-module.exports = router;
